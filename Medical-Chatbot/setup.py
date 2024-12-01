638ed962da9857fb5a0ab08195d828d28d3afe0e
Bhimrao Devidas Pawar <134869229+TVsony@users.noreply.github.com>
2024-12-01 17:56:14 +0530

    Add files via upload

---
+++ Medical-Chatbot/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Bhimrao Pawar',
+    author_email= 'bhimrao.pawar.07@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
